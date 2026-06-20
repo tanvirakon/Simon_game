b025fe406cbdda05daac1839a45f0388e71ced8a
tanvirakon <akon.pt33@gmail.com>
2026-06-20 17:06:00 +0600

    simon finished with ai

--- Simon Game Challenge Starting Files/index.js
+++ Simon Game Challenge Starting Files/index.js
@@ -1,21 +1,30 @@
 var buttonColors = ["red", "green", "yellow", "blue"];
 var level = 0;
 var stringg = "";
+var originalColors = [];
+var acceptingClicks = false;
 
 $(document).keydown(function () {
-  stringg = "";
-  ++level;
-  game(level);
+  if (level == 0) {
+    stringg = "";
+    ++level;
+    game(level);
+  }
 });
 
 function game(level) {
   var i = 0;
+  stringg = "";
+  originalColors = [];
+  acceptingClicks = false;
+
   function playSequence() {
     $("h1").text("Level " + level);
     if (i < level) {
       var randomNumber = Math.floor(Math.random() * 4);
       var desiredColor = buttonColors[randomNumber];
       stringg += desiredColor;
+      originalColors.push(desiredColor);
       console.log(stringg, i);
       if (i == 0) {
         setTimeout(() => {
@@ -30,6 +39,9 @@ function game(level) {
           playSequence(); // Call the function recursively for the next iteration
         }, 500);
       }
+    } else {
+      acceptingClicks = true;
+      mouseClick(level);
     }
   }
   playSequence(); // Start the sequence
@@ -46,38 +58,53 @@ function button(desiredColor) {
   }, 100);
 }
 
-//mouse click
-// function mouseClick(level) {
-//   var colors = "";
-//   for (var i = 0; i < level; i++) {
-//     $(document).click(function (colorPressed) {
-//       colors += colorPressed.target.id;
-//       console.log("inside for " + colors);
-//     });
-//   }
-//   console.log("out for " + colors);
-//   return colors;
-// }
-// var akon = mouseClick(3);
-// console.log(akon);
-
-
+// mouse click
 function mouseClick(level) {
   var colors = "";
   var clicks = 0;
 
-  function handleClick(colorPressed) {
-    colors += colorPressed.target.id;
-    console.log("inside for " + colors);
-    clicks++;
-  }
+  $(".btn").off("click").click(function (colorPressed) {
+    if (acceptingClicks == false) {
+      return;
+    }
 
-  for (var i = 0; i < level; i++) {
-    $(document).click(handleClick);
-  }
-  
-  return colors;
+    var desiredColor = colorPressed.target.id;
+    colors += desiredColor;
+    button(desiredColor);
+    console.log("inside click " + colors);
+
+    if (desiredColor != originalColors[clicks]) {
+      gameOver();
+      return;
+    }
+
+    clicks++;
+
+    if (clicks == level) {
+      acceptingClicks = false;
+      $(".btn").off("click");
+      setTimeout(() => {
+        ++level;
+        game(level);
+      }, 700);
+    }
+  });
 }
 
-var akon = mouseClick(3);
-console.log('akon' + akon);
+function gameOver() {
+  acceptingClicks = false;
+  $(".btn").off("click");
+  $("h1").text("Game Over, Press Any Key to Restart");
+  $("body").addClass("game-over");
+
+  var wrongAudio = new Audio("./sounds/wrong.mp3");
+  wrongAudio.play();
+
+  setTimeout(() => {
+    $("body").removeClass("game-over");
+  }, 200);
+
+  level = 0;
+  stringg = "";
+  originalColors = [];
+}
